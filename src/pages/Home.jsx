94fd62f05dd21a7198dc5db433b708db73a9355c
miguel oliveira <tsmiguelzin2020@gmail.com>
2024-05-17 18:36:30 +0000

    criação dos primeiros estilos

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import { HomeContainer } from './Home.styles'
+
+
+import React from 'react'
+
+export default function Home() {
+  return (
+    <HomeContainer>
+      <h1>home</h1>
+    </HomeContainer>
+  )
+}
